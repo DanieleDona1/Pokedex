58bf2db65008cc40e0b1e1fb35486de868ad15ac
Daniele Dona <d.dona01@gmail.com>
2024-09-08 21:23:39 +0200

    changed code structure

---
+++ script.js
@@ -0,0 +1,265 @@
+let pokemons = [];
+let descriptionObject = [];
+let startLoadPokemons = 0;
+let evolutionChainId = [];
+
+async function onloadfunc() {
+  await loadAndShowPkm();
+}
+
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+const PKMDESCRIPTION_URL = "https://pokeapi.co/api/v2/pokemon-species/";
+
+async function loadAndShowPkm() {
+  onLoadingSpinner();
+
+  let endLoadValue = startLoadPokemons + 10;
+
+  const pkmResponseAsJson = await loadPokemons(startLoadPokemons, endLoadValue);
+  descriptionResponseAsJson = await loadDescriptions(
+    startLoadPokemons,
+    endLoadValue
+  );
+
+  pokemons.push(...createJsonObject(pkmResponseAsJson));
+
+  descriptionObject.push(...createDescriptionObject(descriptionResponseAsJson));
+
+  render();
+  startLoadPokemons = endLoadValue;
+}
+
+function createDescriptionObject(descriptionArray) {
+  return descriptionArray.map((data) => {
+    return {
+      description:
+        data?.flavor_text_entries?.[2]?.flavor_text ||
+        "No description available",
+    };
+  });
+}
+
+async function loadPokemons(start, end) {
+  let pkmPromises = [];
+
+  for (let i = start + 1; i <= end; i++) {
+    const Pokemon_URL = BASE_URL + `${i}`;
+    let response = await fetch(Pokemon_URL);
+    pkmPromises.push(response.json());
+  }
+  return await Promise.all(pkmPromises);
+}
+
+async function loadDescriptions(start, end) {
+  let descriptionPromises = [];
+
+  for (let i = start + 1; i <= end; i++) {
+    const description_URL = PKMDESCRIPTION_URL + `${i}`;
+    let descriptionResponse = await fetch(description_URL);
+    descriptionPromises.push(descriptionResponse.json());
+  }
+  return await Promise.all(descriptionPromises);
+}
+
+function onLoadingSpinner() {
+  document.getElementById("loadContainer").classList.remove("dNone");
+}
+
+function offLoadingSpinner() {
+  document.getElementById("loadContainer").classList.add("dNone");
+}
+
+function createJsonObject(responseAsJson) {
+  return responseAsJson.map((data) => ({
+    ...profilObject(data),
+    ...mainObject(data),
+    ...statsObject(data),
+  }));
+}
+
+function profilObject(data) {
+  return {
+    name: data.name,
+    id: data.id,
+    image: data.sprites.other["official-artwork"].front_default,
+    type: data.types[0].type.name,
+    types: data.types.map((type) => type.type.name),
+  };
+}
+
+function mainObject(data) {
+  return {
+    height: data.height,
+    weight: data.weight,
+    baseExperience: data.base_experience,
+    abilities: data.abilities.map((ability) => ability.ability.name).join(", "),
+  };
+}
+
+function statsObject(data) {
+  return {
+    hp: data.stats[0].base_stat,
+    attack: data.stats[1].base_stat,
+    defense: data.stats[2].base_stat,
+    special_attack: data.stats[3].base_stat,
+    special_defense: data.stats[4].base_stat,
+    speed: data.stats[5].base_stat,
+  };
+}
+
+function render() {
+  let myPokedex = document.getElementById("myPokedex");
+  for (let i = startLoadPokemons; i < pokemons.length; i++) {
+    myPokedex.innerHTML += generateCardTemplate(i);
+  }
+  offLoadingSpinner();
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function openPokemonDetails(i) {
+  document.getElementById("detailViewDialog").classList.remove("dNone");
+  document.getElementById("detailViewDialog").innerHTML =
+    generateDetailCardTemplate(i);
+  document.body.style.overflowY = "hidden";
+  showMain(i);
+}
+
+function filterPokemon() {
+  let input = document.getElementById("search").value;
+
+  if (isValidInput(input)) {
+    processSearchResults(input);
+  } else {
+    updateUIForInvalidInput();
+  }
+}
+
+function isValidInput(input) {
+  return input.length >= 3;
+}
+
+function processSearchResults(input) {
+  const searchResults = document.getElementById("searchResults");
+  searchResults.innerHTML = "";
+  document.getElementById("inputMessage").style.color = "transparent";
+  document.getElementById("btnFooter").disabled = true;
+
+  for (let i = 0; i < pokemons.length; i++) {
+    if (pokemons[i].name.includes(input)) {
+      document.getElementById("myPokedex").classList.add("dNone");
+      document.getElementById("searchResults").classList.remove("dNone");
+      document.getElementById("searchResults").innerHTML +=
+        generateCardTemplate(i);
+    }
+  }
+}
+
+function updateUIForInvalidInput() {
+  document.getElementById("inputMessage").style.color = "red";
+  document.getElementById("myPokedex").classList.remove("dNone");
+  document.getElementById("searchResults").classList.add("dNone");
+  document.getElementById("btnFooter").disabled = false;
+}
+
+function showMain(i) {
+  document.getElementById("statsCharacteristics").innerHTML = "";
+  document.getElementById("evochainCharacteristics").innerHTML = "";
+  document.getElementById("mainCharacteristicsContainer").innerHTML =
+    mainCharacteristicsTemplate(i);
+}
+
+function showStats(i) {
+  document.getElementById("mainCharacteristicsContainer").innerHTML = "";
+  document.getElementById("evochainCharacteristics").innerHTML = "";
+  document.getElementById("statsCharacteristics").innerHTML =
+    statsCharacteristicsTemplate(i);
+}
+
+function closeDialog() {
+  document.getElementById("detailViewDialog").classList.add("dNone");
+  document.getElementById("search").value = "";
+  document.getElementById("myPokedex").classList.remove("dNone");
+  document.getElementById("btnFooter").disabled = false;
+  document.getElementById("searchResults").classList.add("dNone");
+  document.body.style.overflowY = "visible";
+}
+
+function loadMore() {
+  loadAndShowPkm();
+}
+
+async function loadPokemonEvoChain(currentPokemon) {
+  let evolutionChainAsJson = await getEvoulutionChainAsJson(currentPokemon + 1);
+
+  addFirstEvoId(evolutionChainAsJson);
+  addSecondEvoId(evolutionChainAsJson);
+  addThirdEvoId(evolutionChainAsJson);
+
+  showEvo();
+}
+
+function showEvo() {
+  document.getElementById("mainCharacteristicsContainer").innerHTML = "";
+  document.getElementById("statsCharacteristics").innerHTML = "";
+  document.getElementById("evochainCharacteristics").innerHTML = "";
+
+  for (let j = 0; j < evolutionChainId.length; j++) {
+    document.getElementById("evochainCharacteristics").innerHTML +=
+      evoTemplate(j);
+  }
+}
+
+async function getEvoulutionChainAsJson(currentPokemon) {
+  evolutionChainId = [];
+  let evoApi =
+    "https://pokeapi.co/api/v2/pokemon-species/" + `${currentPokemon}`;
+  let response = await fetch(evoApi);
+  let responseAsJson = await response.json();
+  let evolutionChainUrl = responseAsJson.evolution_chain.url;
+  let evolutionChain = await fetch(evolutionChainUrl);
+  return await evolutionChain.json();
+}
+
+function addFirstEvoId(evolutionChainAsJson) {
+  const species = evolutionChainAsJson.chain?.species;
+  if (species) {
+    evolutionChainId[0] = getUrlId(species.url);
+  }
+}
+
+function addSecondEvoId(evolutionChainAsJson) {
+  const species = evolutionChainAsJson.chain?.evolves_to?.[0]?.species;
+  if (species) {
+    evolutionChainId[1] = getUrlId(species.url);
+  }
+}
+
+function addThirdEvoId(evolutionChainAsJson) {
+  const species =
+    evolutionChainAsJson.chain?.evolves_to?.[0]?.evolves_to?.[0]?.species;
+  if (species) {
+    evolutionChainId[2] = getUrlId(species.url);
+  }
+}
+
+function getUrlId(url) {
+  const parts = url.split("/");
+  return parts[parts.length - 2];
+}
+
+function sanitizeDescription(text) {
+  return text.replace(/\f/g, " ");
+}
+
+function previousPkm(i) {
+  openPokemonDetails(i);
+  loadPokemonEvoChain(i);
+}
+
+function nextPkm(i) {
+  openPokemonDetails(i);
+  loadPokemonEvoChain(i);
+}
---
+++ template.js
@@ -0,0 +1,128 @@
+function generateCardTemplate(i) {
+  let pokemonName = capitalizeFirstLetter(pokemons[i].name);
+  let descriptionText = sanitizeDescription(descriptionObject[i].description);
+  return /*html*/ `
+      <div class="card bg_${pokemons[i].type} cardJump">
+        <div class="d_flex_ce_ce">
+          <h2 class="id">#${pokemons[i].id} ${pokemonName}</h2>
+        </div>
+        <img class="cardImg" src="${pokemons[i].image}" alt="pokeimage" />
+        
+        <div id="pokemonstyp" class="pokemonstyp">
+          <div class="typeImg1Container">
+            <img class="typeImg1" src="./typeImg/${pokemons[i].types[0]}.svg">
+            <span class="typeText1">${pokemons[i].types[0]}</span>
+          </div>
+        ${
+          pokemons[i].types[1]
+            ? `
+          <div class="typeImg2Container">
+            <img class="typeImg2" src="./typeImg/${pokemons[i].types[1]}.svg">
+            <span class="typeText2">${pokemons[i].types[1]}</span>
+          </div>
+                  `
+            : ""
+        }
+        </div>
+  
+        <div class="pkmDescription">${descriptionText}</div>
+        <div class="btnPrimary"><button id="btnMoreDetails" class="btnMoreDetails" onclick="openPokemonDetails(${i});">More Details</button>
+            </div>
+        </div>
+        `;
+}
+
+function generateDetailCardTemplate(i) {
+  let pokemonName = capitalizeFirstLetter(pokemons[i].name);
+
+  return /*html*/ `
+      <div class="card detailCard bg_${
+        pokemons[i].type
+      }" onclick="event.stopPropagation();">
+        <div class="d_flex_ce_ce">
+          <h2 class="id">#${pokemons[i].id} ${pokemonName}</h2>
+        </div>
+        <div class="imgContainer"><img class="cardImg" src="${
+          pokemons[i].image
+        }" alt="pokeimage"/>
+        <img onclick="closeDialog()" class="xMark" id="xMark" src="img/xmark.svg" alt="xmark">
+        <img onclick="previousPkm(${i - 1})" class="arrowImgLeft" id="arrowImgLeft" src="img/leftArrow.svg" alt="Left Arrow" class="arrowIcon leftArrow">
+        <img onclick="nextPkm(${i + 1})" class="arrowImgRight" id="arrowImgRight" src="img/arrowRight.svg" alt="Right Arrow" class="arrowIcon rightArrow">
+      </div>
+        
+        <div class="detailInfoContainer">
+          <div class="navigation">
+            <button onclick="showMain(${i})">main</button>
+            <button onclick="showStats(${i})">stats</button>
+            <button onclick="loadPokemonEvoChain(${i})">evo chain</button>
+        </div>
+        <div class="viewCharacteristics">
+            <div class="mainCharacteristicsContainer" id="mainCharacteristicsContainer"></div>
+            <div class="statsCharacteristics" id="statsCharacteristics"></div>
+            <div class="evochainCharacteristics" id="evochainCharacteristics"></div>
+        </div>
+      </div>
+          `;
+}
+
+function mainCharacteristicsTemplate(i) {
+  return /*html*/ `
+              <div class="mainCharacteristics" id="mainCharacteristics">
+                <div>Height:</div>
+                <div>${pokemons[i].height} m</div>
+              </div>
+              <div class="mainCharacteristics" id="mainCharacteristics">
+                <div>Weight:</div>
+                <div>${pokemons[i].weight} kg</div>
+              </div>
+              <div class="mainCharacteristics" id="mainCharacteristics">
+                <div>Base experience:</div>
+                <div>${pokemons[i].baseExperience} exp</div>
+              </div>
+              <div class="mainCharacteristics" id="mainCharacteristics">
+                <div>Abilities:</div>
+                <div>${pokemons[i].abilities}</div>
+              </div>
+  `;
+}
+
+function statsCharacteristicsTemplate(i) {
+  return /*html*/ `
+          <div class="progressContainer">
+                <p>HP: ${pokemons[i].hp}</p>
+                <div class="progressBar bg_bar_${pokemons[i].type}" style="width: ${pokemons[i].hp}%"></div>
+              </div>
+              <div class="progressContainer">
+                <p>ATTACK: ${pokemons[i].attack}</p>
+                <div class="progressBar bg_bar_${pokemons[i].type}" style="width: ${pokemons[i].attack}%"></div>
+              </div>
+              <div class="progressContainer">
+                <p>DEFENSE: ${pokemons[i].defense}</p>
+                <div class="progressBar bg_bar_${pokemons[i].type}" style="width: ${pokemons[i].defense}%"></div>
+              </div>
+              <div class="progressContainer">
+                <p>SPECIAL ATTACK: ${pokemons[i].special_attack}</p>
+                <div class="progressBar bg_bar_${pokemons[i].type}" style="width: ${pokemons[i].special_attack}%"></div>
+              </div>
+              <div class="progressContainer">
+                <p>SPECIAL DEFENSE: ${pokemons[i].special_defense}</p>
+                <div class="progressBar bg_bar_${pokemons[i].type}" style="width: ${pokemons[i].special_defense}%"></div>
+              </div>
+              <div class="progressContainer">
+                <p>SPEED: ${pokemons[i].speed}</p>
+                <div class="progressBar bg_bar_${pokemons[i].type}" style="width: ${pokemons[i].speed}%"></div>
+
+        </div>
+  `;
+}
+
+function evoTemplate(j) {
+  return /*html*/ `
+    <div class="evoContainer">
+      <img onclick="openPokemonDetails(${[evolutionChainId[j] - 1,]})" class="evoImg cardJump" src="${
+    pokemons[evolutionChainId[j] - 1].image
+  }"/>
+      <span>${pokemons[evolutionChainId[j] - 1].name}</span>
+    </div>
+    `;
+}
